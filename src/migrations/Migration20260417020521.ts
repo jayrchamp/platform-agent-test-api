00c418ed6908f30191a49b8e5009397fb13df6a7
jayrchamp <champjr@me.com>
2026-04-16 23:18:57 -0400

    lint

--- src/migrations/Migration20260417020521.ts
+++ src/migrations/Migration20260417020521.ts
@@ -1,13 +1,13 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20260417020521 extends Migration {
-
   override async up(): Promise<void> {
-    this.addSql(`create table "post" ("id" serial primary key, "title" varchar(255) not null, "content" text not null, "created_at" timestamptz not null, "updated_at" timestamptz not null);`);
+    this.addSql(
+      `create table "post" ("id" serial primary key, "title" varchar(255) not null, "content" text not null, "created_at" timestamptz not null, "updated_at" timestamptz not null);`
+    );
   }
 
   override async down(): Promise<void> {
     this.addSql(`drop table if exists "post";`);
   }
-
 }
